c8778fdddf48ea1531f1a0e012aa65950958d529
flendoh <flenditomlgcreations@gmail.com>
2024-07-31 16:31:05 -0500

    chore: Refactor generateMidi.json.ts for improved configuration and prompt

--- src/pages/api/generate-midi.json.ts
+++ src/pages/api/generate-midi.json.ts
@@ -13,25 +13,29 @@ function generateRandomName(): string {
 }
 
 async function generateMidiScheme(userPrompt: string) {
-    let defaultPrompt = 'Compon una pieza musical de alta calidad con variaciones únicas en el BPM, escala, acordes, tempo e instrumentos. Evita la repetición y busca superar una gran cantidad de notas y arreglos musicales. Asegúrate de seguir las siguientes instrucciones y crear una composición original y cautivadora, siguiendo los siguientes estilos: '
+  const defaultPrompt = 'Crea una pieza musical de alta calidad con variaciones únicas en el BPM, escala, acordes, tempo e instrumentos. Evita la repetición y busca superar una gran cantidad de notas y arreglos musicales. Asegúrate de seguir las siguientes instrucciones y crear una composición original y inspiradora, siguiendo los siguientes estilos: ';
 
-    const result = await streamObject({
-      model: google('models/gemini-1.5-pro-latest'),
-      system: "Eres un experto en composición musical. Tu deber es componer piezas musicales de cualquier estilo o género para los usuarios. Tu público objetivo son productores musicales o compositores que necesitan inspiración para comenzar una nueva composición. Tu objetivo es proporcionar una base musical que inspire a los usuarios y les ayude a completar su obra. La composición debe tener una estructura clara, incluyendo introducción, desarrollo y conclusión. Utiliza una variedad de elementos musicales como melodía, armonía, ritmo y dinámica para crear una pieza cautivadora y emotiva. Describe la instrumentación, el tempo, la tonalidad y cualquier técnica musical específica que utilices. Además, proporciona una breve narrativa o concepto que la composición pretende transmitir. Asegúrate de que la composición sea original y demuestre tu experiencia en la creación de piezas musicales únicas e inspiradoras.",
-      schema: MidiSchema,
-      temperature: 0.7,
-      prompt: defaultPrompt + userPrompt
-      
-    })
-    result.toTextStreamResponse()
-    return result.object
+  const systemDescription = 'Eres un experto en composición musical. Tu deber es componer piezas musicales de cualquier estilo o género para los usuarios. Tu público objetivo son productores musicales o compositores que necesitan inspiración para comenzar una nueva composición. Tu objetivo es proporcionar una base musical que inspire a los usuarios y les ayude a completar su obra. La composición debe tener una estructura clara, incluyendo introducción, desarrollo y conclusión. Utiliza una variedad de elementos musicales como melodía, armonía, ritmo y dinámica para crear una pieza cautivadora y emotiva. Describe la instrumentación, el tempo, la tonalidad y cualquier técnica musical específica que utilices. Además, proporciona una breve narrativa o concepto que la composición pretende transmitir. Asegúrate de que la composición sea original y demuestre tu experiencia en la creación de piezas musicales únicas e inspiradoras.';
+
+  const result = await streamObject({
+    model: google('models/gemini-1.5-pro-latest'),
+    system: systemDescription,
+    schema: MidiSchema,
+    temperature: 0.7,
+    prompt: `${defaultPrompt} ${userPrompt}`,
+    frequencyPenalty: 0.5,
+    presencePenalty: 0.5,
+  })
+
+  result.toTextStreamResponse();
+  return result.object
 }
 
-export const POST: APIRoute = async ({request}) => {
+export const POST: APIRoute = async ({ request }) => {
   try {
     const prompt = await request.text()
-    if(prompt.length > 200){
-      return new Response(JSON.stringify('The prompt is too long'), {
+    if (prompt.length > 200 || prompt.length === 0) {
+      return new Response(JSON.stringify('Invalid Prompt'), {
         status: 400,
         headers: {
           'Content-Type': 'application/json',
@@ -39,17 +43,21 @@ export const POST: APIRoute = async ({request}) => {
       });
     }
 
-    const generatedMidi = await generateMidiScheme(prompt)
+    // Generar nombre y fecha en paralelo
+    const [generatedMidi, id] = await Promise.all([
+      generateMidiScheme(prompt),
+      generateRandomName()
+    ]);
 
     const MidiRequest = {
-      id: generateRandomName(),
+      id: id,
       date: new Date(),
       prompt: prompt,
       midiScheme: generatedMidi
-    }
+    };
 
-    const filePath = path.join('/temp', `${MidiRequest.id}.json`);  //cambiar a ./src/assets
-    await writeFile(filePath, JSON.stringify(MidiRequest, null, 2), 'utf-8')
+    const filePath = path.join('tmp', `${MidiRequest.id}.json`);  //cambiar a ./src/assets
+    await writeFile(filePath, JSON.stringify(MidiRequest, null, 2), 'utf-8');
 
     return new Response(JSON.stringify(MidiRequest), {
       status: 200,
@@ -57,7 +65,7 @@ export const POST: APIRoute = async ({request}) => {
         'Content-Type': 'apllication/json',
       },
     });
-  }catch(error){
+  } catch (error) {
     console.error(error);
     return new Response(JSON.stringify('Critical Error'), {
       status: 404,
