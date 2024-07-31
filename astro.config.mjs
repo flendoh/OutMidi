81675dce2eb6283867203740c8835ab77a0f919c
flendoh <flenditomlgcreations@gmail.com>
2024-07-31 13:53:00 -0500

    chore: Update Astro adapter configuration for web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,9 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   integrations: [svelte(), tailwind()],
   vite: {
     ssr: {
